d13b041938c78a8fc68b66d3b73b67b94e383314
Austen Turner <austenpturner@msn.com>
2020-01-12 14:57:23 -0800

    target DOM elements

---
+++ script.js
@@ -0,0 +1,8 @@
+// DOM Elements
+const currentDate = document.getElementById('current-date');
+const hour = document.getElementsByClassName('hour');
+const inputContainer = document.getElementsByClassName('input-container');
+const input = document.getElementsByClassName('user-input');
+const saveBtn = document.getElementsByClassName('save-button');
+
+
