430bdc509c98043222c73abd9ce1bd140e2b8de2
Austen Turner <austenpturner@msn.com>
2020-01-13 10:27:50 -0800

    add function to get schedule from local storage and display it in browser when user loads application

--- script.js
+++ script.js
@@ -6,68 +6,79 @@ const input = document.getElementsByClassName('user-input');
 const saveBtns = document.getElementsByClassName('save-button');
 
 // Schedule variables
-let date = moment().format('dddd, MMMM Do YYYY');
+let day = moment().format('dddd');
+let date = moment().format('MMMM Do YYYY');
 let currentHour = moment().format('ha');
 let inputElement;
 let hourElement;
 let hour;
-let savedPlans;
+let plans;
 let eventHours = [];
 let schedule = [];
 
 // Statement to display current day and date
-currentDate.textContent = `Today is ${date}`;
+currentDate.textContent = `Today is ${day}, ${date}`;
 
 // Statement to change color of present and past hour slots
 for (let i = 0; i < hours.length; i++) {
     if (hours[i].textContent === currentHour) {
         let presentInput = hours[i].nextElementSibling;
         presentInput.classList.add('present-time');
-        
-        let parentLi = hours[i].parentElement;
-        let prevLi = parentLi.previousElementSibling;
-        let prevHour = prevLi.firstElementChild;
-        let prevInput = prevHour.nextElementSibling;
-        prevInput.classList.add('past-time');
+        // TO DO: give all previous inputs class of past-time...
+        if (i !== 0) {
+            let parentLi = hours[i].parentElement;
+            let prevLi = parentLi.previousElementSibling;
+            let prevHour = prevLi.firstElementChild;
+            let prevInput = prevHour.nextElementSibling;
+            prevInput.classList.add('past-time');
+        }
     } 
 }
+
+// Print schedule saved in local storage to screen
+renderSchedule();
 
 // Event listener for save buttons
 for (let i = 0; i < saveBtns.length; i++) {
     saveBtns[i].addEventListener('click', function() {
         inputElement = saveBtns[i].previousElementSibling;
-        savedPlans = inputElement.value;
+        plans = inputElement.value;
         hourElement = inputElement.previousElementSibling;
         hour = hourElement.textContent;
     
         if (inputElement.value === '' && eventHours.indexOf(hour) > -1) {
             deleteEvent();
+            saveSchedule();
            
         } else if (inputElement.value !== '' && eventHours.indexOf(hour) === -1) {
             saveEvent();
+            saveSchedule();
 
         } else if (inputElement.value !== '' && eventHours.indexOf(hour) > -1) {
             updateEvent();
+            saveSchedule();
         }
     })  
 }
 
-// Function for updating calendar
+// Functions for updating calendar
 function saveEvent() {
     eventHours.push(hour);
     let event = {
         time: hour,
-        plans: savedPlans
+        myPlans: plans
     }
     schedule.push(event);
 }
+
 function updateEvent() {
     for (let i = 0; i < schedule.length; i++) {
         if (schedule[i].time === hour) {
-            schedule[i].plans = inputElement.value;
+            schedule[i].myPlans = inputElement.value;
         }
     }
 }
+
 function deleteEvent() {
     for (let i = 0; i < eventHours.length; i++) {
         if (eventHours[i] === hour) {
@@ -77,6 +88,35 @@ function deleteEvent() {
     for (let i = 0; i < schedule.length; i++) {
         if (schedule[i].time === hour) {
             schedule.splice(schedule[i], 1);
+        }
+    }
+}
+
+function saveSchedule() {
+    let savedSchedule = {
+        'date': date,
+        'schedule': schedule
+    };
+    localStorage.setItem('mySchedule', JSON.stringify(savedSchedule));
+}
+
+function renderSchedule() {
+    let lastSchedule = (JSON.parse(localStorage.getItem('mySchedule')).schedule);
+    for (let i = 0; i < lastSchedule.length; i++) {
+        hour = lastSchedule[i].time;
+        eventHours.push(hour);
+        plans = lastSchedule[i].myPlans;
+        let event = {
+            time: hour,
+            myPlans: plans
+        }
+        schedule.push(event);
+    }
+    for (let i = 0; i < hours.length; i++) {
+        let hourToSet = eventHours.indexOf(hours[i].textContent)
+        if (hourToSet > -1) {
+            let inputToSet = hours[i].nextElementSibling;
+            inputToSet.value = schedule[hourToSet].myPlans;
         }
     }
 }
